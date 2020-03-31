aeb8bffc05c54e9fb8ea78432ae4a367e163d3c5
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-03-31 09:33:45 -0300

    feature: configuração do banco de dados

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,23 @@ package br.com.alura.forum.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
+	
+	public List<TopicoDto> lista(){
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação") );
 		List<Topico> topicos = new ArrayList<Topico>();
 		topicos.add(topico);
 		topicos.add(topico);
 		topicos.add(topico);
-		return topicos;
+		return TopicoDto.converter(topicos);
 	}
 }
