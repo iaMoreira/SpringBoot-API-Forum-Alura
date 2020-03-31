75d10feff28e170f47b70e6647d502f772f57f09
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-03-31 16:32:46 -0300

    feature: deletando tópico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,13 @@ public class TopicosController {
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form, UriComponentsBuilder uriComponentsBuilder ){
 		Topico topico = form.atualizar(id, repository);
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping ("/{id}")
+	@Transactional
+	public ResponseEntity<?> atualizar(@PathVariable Long id){
+		repository.deleteById(id);	
+		return ResponseEntity.ok().build();
 	}
 	
 	
