da63e715995a2b9af695965049590d4f87c4106f
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-03-31 10:38:32 -0300

    featura: consulta com filtros

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,18 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+//	@Query("SELECT t FROM TOPICO t WHERE t.curso.name = :nomeCurso")
+//	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 
 }
