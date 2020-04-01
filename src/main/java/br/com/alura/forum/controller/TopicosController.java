19c4cd3221bf87d0532a7d54edbafcd6a7d687dc
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-04-01 13:48:57 -0300

    feature: simplificando ordenação e paginação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,9 +43,9 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+				@PageableDefault(sort = "id", direction = Direction.ASC) Pageable paginacao){
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
 		Page<Topico> topicos;
 		if(nomeCurso == null) {
 			topicos = repository.findAll(paginacao);	
