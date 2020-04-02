f3ae9c78390196436f919d9b8c44a84ec3a29569
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-04-02 09:55:38 -0300

    feature: login de usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$F/dm8G3ZYGyxbNguxVIJ7u7sUrPw6DRNw8amQGzeHulmbdgKyIh5u');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
